543362c771d616b5a099a86fdc44f3d1c1a6cc56
decision-scientists <noahsheldon06@gmail.com>
2020-10-19 19:51:13 +0100

    Adding latest code and files

--- ass_4_data_wrangling.R
+++ ass_4_data_wrangling.R
@@ -13,7 +13,6 @@ head(hSF %>% arrange(Wing))
   
 
 #Creating a df with full names of Species code and Species full name
-
 species_code <- c('RT','SS','CH')
 species_name_full <- c("Red-tailed", "Sharp-shinned", "Cooper's")
 
@@ -32,11 +31,21 @@ hawksWithBMI <- Hawks %>%
   mutate(bird_BMI = 1000 * (Weight/Wing^2)) %>% 
   select(Species,bird_BMI) %>%
   arrange(desc(bird_BMI))
+
+hawksWithBMI %>% filter(!bird_BMI > 100) %>% ggplot(aes(x = bird_BMI,y = Species ,fill = Species)) + xlab("Bird BMI") + geom_violin() + ylab("Species")
+  
+hawksFullName %>% 
+  group_by(Species) %>%
+  summarize(
+    num_rows = n(), min_wing = min(Wing, na.rm = T), avg_wing = mean(Wing, na.rm = T), max_wing = max(Wing, na.rm = T), avg_tail_wing_ratio = mean(Wing/Tail, na.rm = T)
+  )
   
   
-  
-  
-  
+hawksFullName %>% 
+  group_by(Species) %>%
+  summarize(
+    Wing = sum(is.na(Wing)),Weight = sum(is.na(Weight)),Culmen = sum(is.na(Culmen)),Hallux = sum(is.na(Hallux)), Tail = sum(is.na(Tail)), StandardTail = sum(is.na(StandardTail)), Tarsus = sum(is.na(Tarsus)), Crop = sum(is.na(Crop))
+    )
   
   
 
